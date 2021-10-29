1526793db0cd0ae207b467d0425a8843986ee5c8
Pierre <pierre.lagadec@gmail.com>
2021-10-29 18:50:01 +1100

    fetching data from quote API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello")
+let apiQuotes = [];
+
+//show new quote
+function showNewQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+//get quotes from API
+async function getQuotes() {
+    const apiUrl = 'https:/type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        showNewQuote();
+    } catch (error) {
+
+    }
+}
+
+//onload
+getQuotes();
---
+++ script.ts
@@ -0,0 +1,13 @@
+
+
+
+//get quotes from API
+/*async function getQuotes() {
+    const apiUrl = 'https:/type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+    } catch (error) {
+
+    }
+}*/
