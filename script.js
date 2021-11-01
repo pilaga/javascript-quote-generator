fb9524360b708cfb1e764d738f7bf7d9e298fa28
Pierre <pierre.lagadec@gmail.com>
2021-11-01 15:25:26 +1100

    added data loader

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loaderContainer = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//show loading
+function dataLoading() {
+    loaderContainer.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+function dataComplete() {
+    loaderContainer.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 //show new quote
 function showNewQuote() {
+    dataLoading();
+    
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
     //handle author text
@@ -24,10 +39,13 @@ function showNewQuote() {
         quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quote.text;
+
+    dataComplete();
 }
 
 //get quotes from API
 async function getQuotes() {
+    dataLoading();
     const apiUrl = 'https:/type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -48,4 +66,5 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 //onload
 getQuotes();
+
 
